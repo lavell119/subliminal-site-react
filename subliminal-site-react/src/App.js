b8a5a51362600bef236e0ba8f1f1d0e2b1515407
Lee <leewhiz119@gmail.com>
2023-05-05 09:22:31 -0400

    added logo div

--- subliminal-site-react/src/App.js
+++ subliminal-site-react/src/App.js
@@ -3,7 +3,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div className="header"></div>
+      <div className="header">
+        <div className='logo'>LOGO</div>
+        <nav className="nav">
+          <a href="google.com">Home</a>
+          <a href="google.com">How It Works</a>
+          <a href="google.com">Categories</a>
+        </nav>
+      </div>
     </div>
   );
 }
