82e63fa9feaa4d7e9c0d2c4a98975acaae97afcb
Lee <leewhiz119@gmail.com>
2023-05-07 08:23:27 -0400

    added footer

--- subliminal-site-react/src/App.js
+++ subliminal-site-react/src/App.js
@@ -7,11 +7,16 @@ function App() {
         <div className='logo'>LOGO</div>
         <nav className="nav">
           <a href="google.com">Home</a>
-          <a href="google.com">How It Works</a>
+          <a href="google.com">How it Works</a>
           <a href="google.com">Categories</a>
         </nav>
       </div>
-      <div className="sidebar"></div>
+      <div className="sidebar">
+
+      </div>
+      <div className="footer">
+
+      </div>
     </div>
   );
 }
