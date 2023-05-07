e195e397b8516cdd673f4f771b96f4c00b669a8b
Lee <leewhiz119@gmail.com>
2023-05-07 09:08:33 -0400

    created links state and rendered links inside Sidebarlinks

--- subliminal-site-react/src/components/Sidebarlinks.js
+++ subliminal-site-react/src/components/Sidebarlinks.js
@@ -1,8 +1,19 @@
+import react from 'react';
+import { useState } from 'react'
+
 
 function Sidebarlinks() {
+
+  const [links, setLinks] = useState(
+    ["Link1", "Link2", "Link3", "Link4", "Link5", "Link6", "Link7"]
+  )
+
   return (
     <div className="sidebar_links">
-      
+      {links.map((link, index) =>
+      <div className= "link" key={ index}>{link}</div>
+      )
+      }
     </div>
   );
 }
