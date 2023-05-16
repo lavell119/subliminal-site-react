65bbea2f84ed22f8b175c788121ca6040156b993
Lee <leewhiz119@gmail.com>
2023-05-16 14:09:00 -0400

    succesfully rendered individual subliminal categories on each catgory page based on url param

--- subliminal-site-react/src/components/pages/IndividualCategory.js
+++ subliminal-site-react/src/components/pages/IndividualCategory.js
@@ -3,17 +3,27 @@ import { useParams } from 'react-router'
 import SubliminalData from './data.json'
 
 export default function IndividualCategory() {
+  
+    const categories=["Business and Finance", "Health", "Love and Romance", "Physical Changes", "Mental Health", "Self Improvement"]
+
     let param = useParams()
     console.log('testing')
     console.log(param)
+    let id=param.id
+    let output=SubliminalData.filter(data => data.category === categories[id])
+    let outputNames=output.map(output => output.name)
+
+    console.log(output)
 
   return (
     <>
     <div className="red">{param.id}</div>
     <br></br>
-    {SubliminalData.map(data=>
+    {/* {SubliminalData.map(data=>
         <div>{data.name}</div>
-    )}
+    )} */}
+    {outputNames}
+
     </>
   )
 }
