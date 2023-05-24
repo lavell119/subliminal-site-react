fd155a330113a6ae06021910329e8015e7beddff
Lee <leewhiz119@gmail.com>
2023-05-24 13:18:23 -0400

    succesfully set individual subliminal inside IndividualSub component using the url param

--- subliminal-site-react/src/components/pages/IndividualSub.js
+++ subliminal-site-react/src/components/pages/IndividualSub.js
@@ -1,11 +1,23 @@
 import React from 'react'
 import { useParams } from 'react-router'
+import SubliminalData from './data.json'
 
 
 export default function IndividualSub() {
+  let sub
   let param =useParams()
   console.log(param)
+  SubliminalData.forEach(subl => {
+    if(subl.name === param.id){
+      sub=subl.name
+    }
+  })
+  console.log("sub =" + sub)
+
   return (
-    <div>{param.id}</div>
+    <div className="individual_sub">
+      <h2>{param.id}</h2>
+      <p>{sub}</p>
+      </div>
   )
 }
